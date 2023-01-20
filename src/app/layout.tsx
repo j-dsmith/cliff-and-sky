4d768c6899d8a7a6b08e5508e1ce11dbda7f36b8
Jamey Smith <smith.jameyd@gmail.com>
2023-01-20 13:06:36 -0600

    build: add tailwind and set config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,11 @@
-import './globals.css'
+import "@/app/globals.css";
+import Head from "./head";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+    <html>
+      <Head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
