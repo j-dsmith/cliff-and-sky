d07a18bcf263e681b3cb93987290519b15462991
Jamey Smith <smith.jameyd@gmail.com>
2023-01-28 13:50:31 -0600

    style(hero): minor layout tweak

--- src/app/components/hero-section.tsx
+++ src/app/components/hero-section.tsx
@@ -1,16 +1,15 @@
 import Image from "next/image";
 import heroSrc from "@/assets/hero.png";
-import RotatingText from "./rotating-text";
 
 const HeroSection = () => {
   return (
     <section className="isolate flex h-[calc(100vh-80px)] flex-col items-center justify-end">
-      <h1 className="relative z-50 translate-y-8 text-center font-walsheim text-7xl uppercase">
+      <h1 className="relative  z-50 translate-y-8 text-center font-walsheim text-7xl uppercase">
         <span>Cliff</span>
         <br />
         <span className="">& Sky</span>
       </h1>
-      <div className="relative h-full w-full px-8 md:px-16">
+      <div className="relative px-8 md:px-16">
         <Image
           src={heroSrc}
           alt="hero image"
